46a2f4b237e7b7f39b0a8cabe41159d145a5306f
sharkbound <laptopblaster@gmail.com>
2020-01-18 19:46:25 -0600

    new stuff

--- SkyBlock/src/main/kotlin/sharkbound/spigot/skyblock/Extensions.kt
+++ SkyBlock/src/main/kotlin/sharkbound/spigot/skyblock/Extensions.kt
@@ -1,6 +1,8 @@
 package sharkbound.spigot.skyblock
 
+import org.bukkit.Material
 import org.bukkit.command.CommandExecutor
+import org.bukkit.entity.Player
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -12,3 +14,8 @@ fun CommandExecutor.register(name: String) {
 fun <T : Listener> T.register() {
     pluginManager.registerEvents(this, plugin)
 }
+
+fun Player.target(distance: Int = 5000, blocks: Set<Material> = setOf(Material.AIR)) =
+    getTargetBlock(blocks, distance)
+
+val Player.lookLocation get() = target().location
--- SkyBlock/src/main/kotlin/sharkbound/spigot/skyblock/plugin/listeners/JoinListener.kt
+++ SkyBlock/src/main/kotlin/sharkbound/spigot/skyblock/plugin/listeners/JoinListener.kt
@@ -2,6 +2,7 @@ package sharkbound.spigot.skyblock.plugin.listeners
 
 import org.bukkit.Material
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
 import org.bukkit.entity.TNTPrimed
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -9,7 +10,9 @@ import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import sharkbound.commonutils.util.randInt
 import sharkbound.spigot.skyblock.colorFormat
+import sharkbound.spigot.skyblock.lookLocation
 import sharkbound.spigot.skyblock.register
+import sharkbound.spigot.skyblock.target
 
 class JoinListener : Listener {
     init {
@@ -22,21 +25,28 @@ class JoinListener : Listener {
 
     @EventHandler
     fun onInteract(e: PlayerInteractEvent) {
-        if (e.item?.type != null)
+        if (e.item?.type != Material.STICK)
             return
 
-        val number = randInt(1, 10)
+//        val looking = e.player.lookLocation
+//        val range = 20.0
+//        e.player.world.getNearbyEntities(looking, range, range, range).filter { it !is Player }.forEach {
+//            //            println(it.name)
+//            it.velocity = it.velocity.add(looking.subtract(it.location).toVector())
+//        }
+
+        /*val number = randInt(1, 2)
         (1..number).forEach {
             (e.player.world.spawnEntity(
-                e.player.getTargetBlock(setOf(Material.AIR), 1000).location,
+                e.player.target().location,
                 EntityType.PRIMED_TNT
-            ) as TNTPrimed).fuseTicks = 20 * 5
+            ) as TNTPrimed).fuseTicks = 0
         }
 
         e.player.sendMessage(
             colorFormat(
-                "&l&4&oTHE HAND&r has granted you &a&o$number&r &l&4TnT"
+                "&l&4the stick of death&r has granted you &a&o$number&r &l&4TnT"
             )
-        )
+        )*/
     }
 }
